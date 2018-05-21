765cd4888b03ea779dc72841ad9c4e49396a350d
satrio6210 <satrionrndrpnndt362@gmail.com>
2018-05-21 13:19:41 +0700

    update base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/MR-PHP/';
+$config['base_url'] = 'http://localhost/MRP-HP/';
 
 /*
 |--------------------------------------------------------------------------
